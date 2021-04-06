200a4ed4a48f4c3220e37b3818d125bcb380e7e8
Selwyndir <72912872+Selwyndir@users.noreply.github.com>
2021-04-06 15:41:36 +0200

    Update P1.py

--- P1.py
+++ P1.py
@@ -45,7 +45,7 @@ rpi.digitalWrite(pinDirX, rpi.LOW)
 for i in range(2000):
         rpi.digitalWrite(pinStepX, rpi.HIGH)
         rpi.digitalWrite(pinStepX, rpi.LOW)
-    print("P1")
+print("P1")
 
 except:
     print("fin")
