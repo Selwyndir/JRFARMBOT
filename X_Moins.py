feb4679bf440a6155b076fc44472d2c4943ed253
Selwyndir <72912872+Selwyndir@users.noreply.github.com>
2021-04-08 15:21:44 +0200

    Update X_Moins.py

--- X_Moins.py
+++ X_Moins.py
@@ -15,15 +15,15 @@ rpi=ArduinoApi(connection=connection)
 
 #Fin initialisation
 
-#Déclaration des entrées / sorties
+#Declaration des entrées / sorties
 rpi.pinMode(pinStepX, rpi.OUTPUT)
 rpi.pinMode(pinDirX, rpi.OUTPUT)
 rpi.pinMode(pinEnableX, rpi.OUTPUT)
 
-#Désactivation du frein
+#Desactivation du frein
 rpi.digitalWrite(pinEnableX, rpi.LOW)
 
-#Choix de la direction => Sens Négatif
+#Choix de la direction => Sens Negatif
 rpi.digitalWrite(pinDirX, rpi.HIGH)
 
 #Recule de 5mm sur X+ <=> 1 tour de MotX <=> 200 pas pour le moteur
