a1278198d1ff259f25d80c2d7094267f59de5638
Selwyndir <72912872+Selwyndir@users.noreply.github.com>
2021-04-09 15:10:05 +0200

    Add files via upload

---
+++ W/lumiere_ON.py
@@ -0,0 +1,15 @@
+#lumiere_ON.py 
+# Dahan--Lefevre Sullivan
+Lampe = 9
+LUM_PIN = 57
+
+from nanpy import (ArduinoApi, SerialManager)
+from time import sleep
+
+connection = SerialManager(device='/dev/ttyACM0')
+rpi=ArduinoApi(connection=connection)
+
+rpi.pinMode(Lampe, rpi.OUTPUT)
+rpi.pinMode(LUM_PIN, rpi.INPUT)
+
+rpi.digitalWrite(Lampe, rpi.HIGH)
