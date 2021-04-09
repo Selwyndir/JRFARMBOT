baba486abebffd6af22e72b3f51e1a0ac86bb676
Selwyndir <72912872+Selwyndir@users.noreply.github.com>
2021-04-09 15:11:35 +0200

    Add files via upload

---
+++ Automatisation/Python/LumièreAuto.py
@@ -0,0 +1,20 @@
+#LumiereAuto.py 
+# Dahan--Lefevre Sullivan
+Lampe = 9
+LUM_PIN = 57
+
+from nanpy import (ArduinoApi, SerialManager)
+from time import sleep
+
+connection = SerialManager(device='/dev/ttyACM0')
+rpi=ArduinoApi(connection=connection)
+
+rpi.pinMode(Lampe, rpi.OUTPUT)
+rpi.pinMode(LUM_PIN, rpi.INPUT)
+
+Lumino = rpi.digitalRead(LUM_PIN)
+while True :
+    if Lumino = 1 : 
+    rpi.digitalWrite(Lampe, rpi.LOW)
+    elif Lumino = 0:
+    rpi.digitalWrite(Lampe, rpi.HIGH)
